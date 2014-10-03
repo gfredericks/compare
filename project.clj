4d99345d262d81a72cffdaca8a1b2f738d7d6c41
Gary Fredericks <fredericksgary@gmail.com>
2014-10-03 12:31:14 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/compare "0.1.0-SNAPSHOT"
+(defproject com.gfredericks/compare "0.1.0"
   :description "Helper functions for clojure.core/compare"
   :url "https://github.com/gfredericks/compare"
   :license {:name "Eclipse Public License"
